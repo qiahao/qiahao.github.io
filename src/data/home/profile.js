4a8cd8ae4bd620c21013f71662e298de6aef0602
qiahao <404546074@qq.com>
2017-10-17 09:33:41 +0800

    修改、打包

--- src/data/home/profile.js
+++ src/data/home/profile.js
@@ -14,20 +14,21 @@ export default {
         {
             title: '技术能力：', 
             list: [
-                '熟练使用vue/ iview/ angular1.x/ ionic/ axios，了解MV*模式；',
-                '熟练使用javascript，熟悉ES6, 了解nodejs；',
+                '熟练使用vue/ iview/ angular1.x/ ionic/ axios，了解MV*模式，了解vue、angular1.x 源码；',
+                '熟练使用javascript、es6；了解nodejs；',
                 '熟练掌握html/ css/ jquery/ zepto/ bootstrap等技术，能快速、清晰的编辑前端结构、样式、效果；',
-                '熟练使用webpack/ gulp/sass/jadejs/gulp/ps/sublime/inteliJ/webStrom 等前端工具；',
+                '熟悉前端自动化构建，熟练使用webpack、gulp构建工具；',
+                '熟练使用sass/less/jadejs、ps/sublime/vscode/inteliJ/webStrom 等前端工具；',
                 '熟悉pc、移动浏览器兼容；有能力解决浏览器兼容问题；',
-                '对后端有一定了解',
+                '对后端和后端语言有一定了解',
             ]
         },
         {
             title: '核心优势：', 
             list: [
                 '善于规划、管理个人工作、协同团队工作需求；',
-                '较强的沟通能力、较强的学习能力、喜欢钻研技术；',
-                '能独立开发、适应团队开发、能主动寻求办法解决技术的问题；',
+                '较强的沟通能力、较强的学习能力、对新技术富有激情、有团队意识；',
+                '能独立开发、适应团队开发、主动寻求办法解决技术的问题；',
                 '有一定的前端优化经验，针对网站实际情况，提出可行性前端优化方案；',
                 '有移动端、pc端、混合app开发经验；对应不同适配需求，能给出相应解决方案；',
                 '良好的编程习惯和团队协作精神、工作积极向上、自主学习能力较强、较强的沟通协调能力；',
@@ -37,7 +38,8 @@ export default {
         {
             title: '正在学习：', 
             list: [
-                'node.js/ express/ python；',
+                'node.js + express web开发',
+                'python、typeScript',
                 '服务端相关知识；'
             ]
         }
