e75bb20fdb48dbd84549d10a1e8231e80b303b3b
qiahao <404546074@qq.com>
2018-11-19 11:18:40 +0800

    update

--- src/data/home/profile.js
+++ src/data/home/profile.js
@@ -1,27 +1,27 @@
 export default {
-    name: '甘乾泰',
-    title: '前端工程师',
-    yearsOfWorking : '4.5年',
-    sex: '男',
-    birth: '1989-05',
-    phone: '15880153020',
-    email: '404546074@qq.com',
-    qq: '404546074',
-    graduation: '重庆交通大学',
-    comeFrom: '福建 宁德',
-    image: 'xx',
+  name: '甘乾泰',
+  title: '前端工程师',
+  yearsOfWorking : '5.5年',
+  sex: '男',
+  birth: '1989-05',
+  phone: '15880153020',
+  email: '404546074@qq.com',
+  qq: '404546074',
+  graduation: '重庆交通大学',
+  comeFrom: '福建 宁德',
+  image: 'xx',
     // 自我介绍 分段
-    introduce:[
-        {
+  introduce:[
+    {
             // title: '个人评价：', 
-            list: [
-                '长期从事前度开发，可快速编写高效、可维护性代码，重视团队代码风格统一，主导结构、样式规划，UI组件开发维护、前端自动化构建、提高前端开发效率；',
-                '熟悉前端主流MV*框架 Vue/angular/react，了解前端自动化构建工具webpack/gulp,其中熟练掌握Vue/angular1.x，并有过多个项目实践，目前主要从事Vue项目构建、可维护性组件开发、项目优化、业务逻辑开发；',
-                '专注于前端技术框架选型、二次开发、工具使用、共同制定开发约定，最大限度降低开发、维护成本。涉及技术包括：vue、vue-router、vuex、iview、axios、angular1.x、ionic、es6、jquery、bootstrap、sass、less、webpack、gulp、github',
-                '掌握扎实的html、css、js基础，对项目优化有一定经验，能对移动端、pc端兼容性作出有效性解决方案。',
-                '学习能力较强，并保持持续性学习，对前端前沿技术充满兴趣，乐于和团队共同研究、学习、分享，对node、express以及后端语言java、客户端语言swift有一定了解',
-            ]
-        },
+      list: [
+        '长期从事前度开发，可快速编写高效、可维护性代码，重视团队代码风格统一，主导结构、样式规划，UI组件开发维护、前端自动化构建、提高前端开发效率；',
+        '熟悉前端主流MV*框架 Vue/angular/react，了解前端自动化构建工具webpack/gulp,其中熟练掌握Vue/angular1.x，并有过多个项目实践，目前主要从事Vue项目构建、可维护性组件开发、项目优化、业务逻辑开发；',
+        '专注于前端技术框架选型、二次开发、工具使用、共同制定开发约定，最大限度降低开发、维护成本。涉及技术包括：vue、vue-router、vuex、iview、axios、angular1.x、ionic、es6、jquery、bootstrap、sass、less、webpack、gulp、github',
+        '掌握扎实的html、css、js基础，对项目优化有一定经验，能对移动端、pc端兼容性作出有效性解决方案。',
+        '学习能力较强，并保持持续性学习，对前端前沿技术充满兴趣，乐于和团队共同研究、学习、分享，对node、express以及后端语言java、客户端语言swift有一定了解',
+      ]
+    },
         // {
         //     title: '技术能力：', 
         //     list: [
@@ -35,13 +35,13 @@ export default {
         //         '对后端和后端语言有一定了解',
         //     ]
         // },
-        {
-            title: '近期正在学习：', 
-            list: [
-                '全栈开发： 学习使用 node.js + express web服务端开发，通过掌握exprss熟悉服务端知识点；',
-                '掌握react及混合app开发：目前能使用react 完成前端项目开发，正在学习react相关库及框架和swift等iOS基础知识；',
+    {
+      title: '近期正在学习：', 
+      list: [
+        '全栈开发： 学习使用 node.js + express web服务端开发，通过掌握exprss熟悉服务端知识点；',
+        '掌握react及混合app开发：目前能使用react 完成前端项目开发，正在学习react相关库及框架和swift等iOS基础知识；',
                 // '学习客户端语言swift，通过对ios客户端的进一步了解，用于完成 app hybrid 开发；'
-            ]
-        }
-    ]
+      ]
+    }
+  ]
 }
